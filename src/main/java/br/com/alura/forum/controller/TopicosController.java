72be72970ee08873fbc89fbcdf760a04457bfb36
Lidianne Romão <83047245+lidiannerb@users.noreply.github.com>
2021-06-19 19:32:10 -0300

    Finalizado

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -20,6 +20,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/topicos") //URl definida apenas uma vez em cima da classe, indicando que todos os métodos começam com "/topicos" e os verbos são definidos em cada método
@@ -31,7 +32,8 @@ public class TopicosController {
     @Autowired
     private CursoRepository cursoRepository;
 
-    @GetMapping    //método para listar o curso
+    //método para listar os tópicos do banco de dados 
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso){
         if (nomeCurso==null){
             List<Topico> topicos = topicoRepository.findAll();
@@ -41,8 +43,9 @@ public class TopicosController {
             return TopicoDto.converter(topicos);
         }
     }
-
+    //Método para cadastrar novo tópico no banco de dados
     @PostMapping  //O @RequestBody informa ao Spring que ele deve pegar o TopicoForm do corpo da requisição e não como parâmetro da URL
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar (@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder){    //TopicoForm > classe que indica que são dados do cliente que chegam para a API
         Topico topico = form.converter(cursoRepository);
         topicoRepository.save(topico);   // Iremos receber um form, portando temos que converte-lo para um topico
@@ -51,18 +54,39 @@ public class TopicosController {
         return ResponseEntity.created(uri).body(new TopicoDto(topico));
     }
 
+    //Método para trazer os detalhes do tópico do banco de dados
     @GetMapping("/{id}")  //indica que parte da URL é dinamica, e chamamos essa parte de id
-    public DetalhesDoTopicoDto detalhar(@PathVariable Long id){
-        Topico topico = topicoRepository.getOne(id);
-        return new DetalhesDoTopicoDto(topico);
+    public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id){
+        Optional<Topico> topico = topicoRepository.findById(id);
+        if (topico.isPresent()){
+            return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+        }
+            return ResponseEntity.notFound().build();
 
     }
 
-    @PutMapping("/{id}")  //PUT: atualiza todas as informações PATCH: atualiza uma pequena atualização
+    //Método para atualizar informações no banco > PUT: atualiza todas as informações PATCH: atualiza uma pequena atualização
+    @PutMapping("/{id}")
     @Transactional  //dispara o commit no banco de dados
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
-        Topico topico = form.atualizar(id, topicoRepository);
-
-        return ResponseEntity.ok(new TopicoDto(topico));
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()){
+            Topico topico = form.atualizar(id, topicoRepository);
+            return ResponseEntity.ok(new TopicoDto(topico));
+        }
+        return ResponseEntity.notFound().build();
     }
+
+    //Método para apagar dados do banco de dados
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id){
+        Optional<Topico> optional = topicoRepository.findById(id);
+        if (optional.isPresent()){
+            topicoRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
